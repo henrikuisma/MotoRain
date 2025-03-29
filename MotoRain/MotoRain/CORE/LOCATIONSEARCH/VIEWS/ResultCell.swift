4cfeb38376c641992be1d485a05a96a973d173cf
Henri <henri.mkuisma@gmail.com>
2025-03-29 17:29:28 +0200

    Rename HomeView to ContentView and add LocationSearch components

---
+++ MotoRain/MotoRain/CORE/LOCATIONSEARCH/VIEWS/ResultCell.swift
@@ -0,0 +1,18 @@
+//
+//  ResultCell.swift
+//  MotoRain
+//
+//  Created by Henri Kuisma on 29.3.2025.
+//
+
+import SwiftUI
+
+struct ResultCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultCell()
+}
