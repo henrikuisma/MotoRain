b429f2d17064cc92ba30080734aa84a59f947e8b
Henri <henri.mkuisma@gmail.com>
2025-04-10 23:09:44 +0300

    Add MapViewState enum to manage map view UI state transitions

---
+++ MotoRain/MotoRain/CORE/MODELS/MapViewState.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewState.swift
+//  MotoRain
+//
+//  Created by Henri Kuisma on 10.4.2025.
+//
+
+import Foundation
+
+
+enum MapViewState {
+    case noInput
+    case searchingForLocation
+    case locationSelected
+}
